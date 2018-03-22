cea5389ab1f0b1b5a0cf00d236df4f48df364d00
Jiayang Li <gscsdyang@gmail.com>
2018-03-22 15:10:24 -0400

    add more stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
-var HelloWorld = artifacts.require("./helloworld.sol")
+var HelloWorld = artifacts.require("./helloworld.sol");
+var ScoreStore = artifacts.require("./ScoreStore.sol");
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(HelloWorld);
+  deployer.deploy(ScoreStore);
 };
